25a385cbeb5c92be9b6fdddabb28fa18fdacd043
Micaella <micaellatosta@gmail.com>
2025-12-05 20:16:50 -0300

    feat

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,11 +6,11 @@ class Database{
     }
     init() {
         this.db = new Sequelize({
-            database: 'agenda',
-            host: 'localhost',
-            password:'',
-            username: 'root',
-            dialect:'mysql'
+            database: 'atividade_oxdd',
+            host: 'dpg-d4plp72dbo4c73bd91ng-a',
+            password:'YL2d8jWClEHmBgVuVt9XFjBzNuzutxxn',
+            username: 'atividade_oxdd_user',
+            dialect:'postgres'
            
         })
     }
